3d023b67aab8fe2e1ee94bc30aaefa84a5519bcf
elcinp <elcinpars1@gmail.com>
2022-10-01 16:29:30 +0300

    AuthContextProvider created and firebase added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import AuthContextProvider from "./context/AuthContextProvider";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <AuthContextProvider>
+        <Navbar />
+      </AuthContextProvider>
     </div>
   );
 }
