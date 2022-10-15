4803ccb963c42e809208e73c03a596b9a21021b7
elcinp <elcinpars1@gmail.com>
2022-10-15 14:50:24 +0300

    PrivateRouter created

---
+++ src/router/PrivateRouter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { useAuth } from "../context/AuthContextProvider";
+
+const PrivateRouter = (props) => {
+    let { currentUser } = useAuth();
+
+    //!Just for testing purpose
+    // currentUser = {
+    //   email: "a@gmailcom",
+    // };
+
+  return currentUser ? (
+    <Route path={props.path} component={props.component} />
+  ) : (
+    <Redirect to="/login" />
+  );
+};
+
+export default PrivateRouter;
